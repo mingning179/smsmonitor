e199a8e835df8b1323de998c780e76eb9bafe30f
mingning179 <mingning179@qq.com>
2025-03-17 17:38:21 +0800

    build: 更新应用版本号
    - 将 versionCode 从 1 修改为 2
    - 将 versionName 从 "1.0" 修改为 "1.1"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.nothing.sms.monitor"
         minSdk = 25
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
